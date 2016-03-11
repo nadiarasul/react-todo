1ae246379e3814d0045502900f166cf309138577
Kristen Spencer <kristen@kristenspencer.ca>
2016-03-10 22:01:19 -0500

    added comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,44 @@
 var gulp = require('gulp'),
+// these will handle converting our ES6 and JSX into browser-friendly JS 
 		browserify = require('browserify'), 
-		source = require('vinyl-source-stream'),
 		babelify = require('babelify'),
+//this package converts what browserify gives us into something gulp can read
+		source = require('vinyl-source-stream'),
+// here are all our packages to compile sass and minify our code		
+		sass = require('gulp-sass'),
 		sourcemaps = require('gulp-sourcemaps'),
 		notify = require('gulp-notify'),
 		buffer = require('gulp-buffer'), 
 		uglify = require('gulp-uglify'),
-		sass = require('gulp-sass'),
 		minifyCSS = require('gulp-minify-css'),
 		concat = require('gulp-concat'),
 		autoprefixer = require('gulp-autoprefixer');
 		plumber = require('gulp-plumber');
 
 
+
+gulp.task('js', function() {
+	// take the code in our react app and...
+	browserify('./app.jsx')
+	//transform ES6 and JSX into JavaScript
+		.transform(babelify,{presets: ["es2015", "react"]})
+		//compile it into one file and tell us if there is any errors
+		.bundle().on('error', notify.onError({
+      title: "JSX Error",
+      message: "<%= error.message %>"
+    }))
+    // minify it 
+		.pipe(source('app.min.js'))
+    .pipe(buffer())
+    // allow for console errors to point us to the problem in our working file
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    // put the compiled JS into our production folder 
+		.pipe(gulp.dest('app/js'));
+});
+
+// styles task not needed for react, but maybe you want this to look like something
 gulp.task('styles', function() {
 	return gulp.src('./sass/**/*.scss')
 		.pipe(plumber({
@@ -27,25 +53,13 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('./app/styles'));
 });
 
-gulp.task('js', function() {
-	browserify('./app.jsx')
-		.transform(babelify,{presets: ["es2015", "react"]})
-		.bundle().on('error', notify.onError({
-      title: "JSX Error",
-      message: "<%= error.message %>"
-    }))
-		.pipe(source('app.min.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('app/js'));
-});
 
 
+// watch sass folder for style changes, and jsx file for JS changes
 gulp.task('watch', function(){
 	gulp.watch('sass/**/*.scss', ['styles']);
 	gulp.watch('./app.jsx', ['js']);
 	});
 
+// run all of these tasks when gulp is started, and then run the watch task
 gulp.task('default', ['styles', 'js', 'watch']);
